7aba205dc4fe7123647900b1660d4cdbd4bda265
KacperOmen <komen1@stu.vistula.edu.pl>
2026-04-19 20:28:35 +0200

    68148 zad4: Dodanie animacji przy kliknięciu przycisku + zmiana koloru przycisku dla obydwu motywów

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let theme = document.getElementById("theme-style")
+const slider = document.getElementById("slider")
 
 function changeColor68148() {
     const href = theme.getAttribute("href")
-
-    href === "./red.css" ? theme.setAttribute("href", "./green.css") : theme.setAttribute("href", "./red.css")
+    if (href === "./red.css") {
+        theme.setAttribute("href", "./green.css")
+        slider.style.transform = "translateX(30px)"
+    }
+    else {
+        theme.setAttribute("href", "./red.css")
+        slider.style.transform = "translateX(0)"
+    }  
 }
