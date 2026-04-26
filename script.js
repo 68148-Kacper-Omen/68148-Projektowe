d23b1a059e3143e0632bd2cd70ce928e72160d61
KacperOmen <komen1@stu.vistula.edu.pl>
2026-04-26 16:45:25 +0200

    68148 zad5: Dodanie walidacji formularza w javascript + stylowanie błędów

--- script.js
+++ script.js
@@ -24,3 +24,75 @@ function hideOrShowSection() {
         hideOrShowBtn.textContent = "+"
     }
 }
+
+// Walidacja formularza
+const form = document.getElementById("messageForm")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const name = document.getElementById("name")
+    const surname = document.getElementById("surname")
+    const email = document.getElementById("email")
+    const message = document.getElementById("message")
+
+    const nameError = document.getElementById("nameError")
+    const surnameError = document.getElementById("surnameError")
+    const emailError = document.getElementById("emailError")
+    const messageError = document.getElementById("messageError")
+
+    let isValid = true
+
+    // Imię
+    if (name.value.trim() === "") {
+        nameError.textContent = "Imię jest wymagane"
+        isValid = false
+    }
+    else if (/\d/.test(name.value)) {
+        nameError.textContent = "Imię nie może zawierać cyfr"
+        isValid = false
+    }
+    else {
+        nameError.textContent = ""
+    }  
+
+    // Nazwisko
+    if (surname.value.trim() === "") {
+        surnameError.textContent = "Nazwisko jest wymagane"
+        isValid = false
+    }
+    else if (/\d/.test(surname.value)) {
+        surnameError.textContent = "Nazwisko nie może zawierać cyfr"
+        isValid = false
+    }
+    else {
+        surnameError.textContent = ""
+    }
+
+    // Email
+    if (email.value.trim() === "") {
+        emailError.textContent = "Email jest wymagany"
+        isValid = false
+    }
+    else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
+        emailError.textContent = "Podaj poprawny adres email"
+        isValid = false
+    }
+    else {
+        emailError.textContent = ""
+    }
+
+    // Wiadomość
+    if (message.value.trim() === "") {
+        messageError.textContent = "Wiadomość jest wymagana"
+        isValid = false
+    }
+    else {
+        messageError.textContent = ""
+    }
+
+    console.log(isValid)
+    if (isValid) {
+        alert("Wiadomość wysłana!")
+        form.submit()
+    }
+})
